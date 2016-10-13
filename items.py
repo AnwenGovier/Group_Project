c38aac9f5a60dcbf65076a40f73286946a2e1892
LewisSarcasm <lewis36@hotmail.co.uk>
2016-10-13 15:24:42 +0100

    Added items.py

---
+++ items.py
@@ -0,0 +1,134 @@
+item_pistol = {
+	"id": "pistol",
+
+	"name": "Pistol",
+
+	"damage": 90,
+
+	"heal": 0,
+
+	"description": 
+	"""Used to blast fools!"""
+}
+
+
+item_biscuit = {
+	"id": "biscuit",
+
+	"name": "a biscuit",
+
+	"damage": 0,
+
+	"heal": 10,
+
+	"description": 
+	"""A delicious biscuit that has regenerative properties."""
+}
+
+item_broom = {
+	"id": "broom",
+
+	"name": "a broom", 
+
+	"damage": 10,
+
+	"heal": 0,
+
+	"description": 
+	"""A broom used to sweep the floor. Can also used as a deadly weapon or as a dancing partner."""
+}
+
+item_kitchen_knife = {
+	"id": "kitchen knife",
+
+	"name": "a kitchen knife",
+
+	"damage": 0,
+
+	"heal": 0,
+
+	"desciption":
+	"""A sharp kitchen knife used to chop up the meanest carrots! 
+	Side note: Can also be used slice an enemy in two."""
+}
+
+item_alcohol = {
+	"id": "alcohol",
+
+	"name": "an alcoholic drink",
+
+	"damage": 0,
+
+	"heal": -5,
+
+	"description":
+	"""A refreshing alcoholic drink. One sip and you will be intoxicated."""	
+}
+
+item_tablets = {
+	"id": "tablets",
+
+	"name": "tablets",
+
+	"damage": 0,
+
+	"health": 25,
+
+	"description":
+	"""These tablets will regenerate your health!"""
+}
+
+item_secret_key = {
+	"id": "secret key",
+
+	"name": "a secret key",
+
+	"damage": 0,
+
+	"health": 0,
+
+	"description":
+	"""This secret key will unlock a door that will let you skip part of a level."""
+}
+
+
+item_room5_key = {
+	"id": "room 5 key",
+
+	"name": "a key for room 5",
+
+	"damage": 0, 
+
+	"health": 0,
+
+	"description": 
+	"""This key will unlock room 5 on level 2."""
+
+}
+
+item_room3_key = {
+	"id": "room 3 key",
+
+	"name": "a key for room 3",
+
+	"damage": 0,
+
+	"health": 0,
+
+	"description":
+	"""This key will unlock room 3 on level 2."""
+}
+
+item_second_level_key = {
+	"id": "second level key",
+
+	"name": "a key for the second level",
+
+	"damage": 0,
+
+	"health": 0,
+
+	"description":
+	"""This key will unlock the stairs to level 2."""
+}
+
