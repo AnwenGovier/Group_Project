621004bd270285db17249d18af742e786f2fc871
AnwenGovier <GovierA3@cardiff.ac.uk>
2016-10-18 11:11:43 +0100

    added comments and doctests

--- game.py
+++ game.py
@@ -59,7 +59,7 @@ def print_a_list_of_inventory_items(items):
 	The difference is the words that are printed with the items.
 	e.g.
 	>>> print_a_list_of_inventory_items(inventory)
-	(no output)
+	You have a pistol.
     """
 	list_of_items_in_inventory = create_a_list_of_items(items)
 	if len(list_of_items_in_inventory) == 0:
@@ -138,7 +138,22 @@ def print_the_exits_avaliable_to_user(direction, leads_to):
 
 def print_menu_of_items_and_exits(exits, room_items, inventory_items):
 	"""
-	Please add test
+	This function displays to the user a list of avaliable exits that they can take to move
+	out of their current room. It will also display all of the items in the room in the following
+	manner:
+	TAKE <ITEM ID> to take <item name>.
+	It will also display all of the items in their inventory that they can drop using the following code:
+	DROP <ITEM ID> to drop <item name>.
+	It splits all of the items in the inventory so that they can be displayed individually to the user.
+	e.g.
+	The actions avaliable to the player in the batr are as follows:
+	You can:
+	GO EAST to the lobby.
+	GO NORTH to the kitchen.
+	TAKE BISCUIT to take a biscuit.
+	TAKE ALCOHOL to take an alcoholic drink.
+	DROP PISTOL to drop a pistol.
+	What do you want to do?
 	"""
 
 	print("You can:")
@@ -159,15 +174,39 @@ def print_menu_of_items_and_exits(exits, room_items, inventory_items):
 			if x == item_name:
 				print('DROP ' + items['id'].upper() + ' to drop ' + items['name'] + '.')
 
+	print("What do you want to do?")
+
 def is_inputs_a_valid_exit(exits, chosen_exit):
 	"""
-	ADD TEST
+	The code in this function compares the players chosen exit with the exits in the
+	dictionary in game_map.py. If the chosen exit exists then the exit is valid and
+	Trus is returned. Else False is returned.
+	It is assumed that the input has already been normalised before this function is
+	run.
+	e.g.
+	>>> is_inputs_a_valid_exit(rooms["Lobby"]["exits"], "west")
+	True
+	>>> is_inputs_a_valid_exit(rooms["Kitchen"]["exits"], "east")
+	False
+	>>> is_inputs_a_valid_exit(rooms["Kitchen"]["exits"], "south")
+	True
 	"""
 	return chosen_exit in exits
 
 def go(direction):
 	"""This function allows the player to traverse the world by inputting 
-	a direction"""
+	a direction. If the direction is valid the current room is updated to reflect the
+	players movement from the previous room.
+	It also checks that the room is not locked. If it is then it stops the user from entering
+	and displays to the user that the room is locked and they didn't answer the question correctly.
+	If the room they have now entered is the roof then it will run a seperate py flie Battle.py
+	which will allow the player to fight the boss.
+	If the new current room is the Stairs to the first floor then again a seperate file is fun,
+	stairwellbattle.py, which makes the user fight a man in order to go to the next floor.
+	If any of these conditions are not met then the following is printed:
+	You cannot go there!
+	"""
+
 	global current_room
 	if is_inputs_a_valid_exit(current_room['exits'], direction):
 		current_room_check = room[current_room['exits'][direction]]
@@ -194,7 +233,8 @@ def go(direction):
 
 def question(current_room):
 	"""This function will ask the user a question that will need to be answered 
-	correctly to unlock the room."""
+	correctly to unlock the room.
+	If the answer is incorrect then they cannot open the door"""
 	if current_room == room["the stairs to the first floor"]:
 		#This question is number 1 
 		print("What colour is the chef's hat?")
@@ -228,10 +268,16 @@ def question(current_room):
 	elif current_room == room["the secret door"]:
 		#This question is number 6
 		print("What is in the laundry?")
-		print("Spagetti\n", "Shoes\n", "Bucket\n", "Shirts\n", "To exit question enter: EXIT")
+		print("Spaghetti\n", "Shoes\n", "Bucket\n", "Shirts\n", "To exit question enter: EXIT")
 		return answer(6, current_room)
 
 def answer(question_number, current_room):
+	'''
+	This function checks to see if the users input matches the correct answer. If it does then they
+	are allowed to move into the room or up the stairs. If it is not correct then the following is 
+	displayed:
+	That is the wrong answer, try again!
+	'''
 	if question_number == '1':
 		while True:
 			user_ans = input(">>")
@@ -288,7 +334,7 @@ def answer(question_number, current_room):
 		while True:
 			user_ans = input(">>")
 			user_ans = user_input_normalisation(user_ans)
-			if user_ans == 'spagetti':
+			if user_ans == 'spaghetti':
 				return True
 				break
 			elif user_ans == 'exit':
@@ -296,7 +342,9 @@ def answer(question_number, current_room):
 
 
 def locked(current_room):
-	"""This function checks if the room is locked or not"""
+	"""This function checks if the users current room is locked or not.
+	If it is then it will ask the user a question else if it unlocked (False) then the
+	player is allowed to enert the room."""
 
 	for x in current_room:
 		if x == 'locked':
@@ -308,42 +356,101 @@ def locked(current_room):
 
 
 def execute_program(current_room):
-	"""This function will exectute another program if a certain room is entered. If there is 
+	"""
+	This function will exectute another program if a certain room is entered. If there is 
 	no program attatched to that room nothing will be executed and the current room is 
-	returned."""
+	returned.
+	"""
 
 	
+def is_item_in_list(item, items):
+    """
+    This function compares the users input with the items that are currently avaliable in the room.
+    If it can find then item then it returns True else it will return False.
+    For example if the function checked to see if there is a biscuit in the alley then it would
+    return True but if it checked that a broom is in the alley, then False would be returned.
+    """
+    for i in items:
+        id = i["id"]
+        if item == id:
+            return True
+    return False
 
+def remove_item_from_current_room(item_id, items):
+    """
+	This function removes the requested item from the current room. 
+	It will then add the item into the players inventory. 
+	The changes will be displayed in the print_menu_of_items_and_exits function.
+    """
+    items_in_room = []
+    for i in items:
+        id = i["id"]
+        if item_id == id:
+            inventory.append(i)         
+        else:
+            items_in_room.append(i)
+    current_room["items"] = items_in_room
+
+    return False
+       
+
+def is_item_in_inventory(item, items):
+    """
+    This function compares the users input with the items that they have in their inventory.
+    It it can find the requested item then True is return. Else False is returned.
+	"""
+    for i in items:
+        id = i["id"]
+        if item == id:
+            return True
+    return False
+
+
+def remove_item_from_inventory(item_id, items):
+
+    """
+	This function removes the requested item in the players inventory and appends it to the
+	list of items in the players current room.
+	The changes are displayed in the print_menu_of_items_and_exits function for the player.
+	"""
+	global inventory
+	items_in_inventory = []
+	for i in items:
+		id = i["id"]
+		if item_id == id:
+			current_room["items"].append(i)
+		else:
+			items_in_inventory.append(i)
+	inventory = items_in_inventory
+	return False
 
 def take_an_item(item_id):
-	"""This function allows the user to take an item from the
-	current room"""
+	"""
+	This function allows the user to take an item from the current room.
+	It runs two seperate functions, one to make sure the chosen item is valid and one to
+	move the item from the room into the players inventory.
+	If it cannot do any of these things then You cannot take that is displayed."""
 
-	#small note:- i dont think the 'you cannot take this' works 
-
-	for item in current_room['items']:
-		if item_id != item['id'] and current_room['items']:
-			pass
-		elif item_id == item['id']:
-			current_room['items'].remove(item)
-			inventory.append(item)
-			print(item['name'] + " added to your inventory.")
-		else:
-			print("You cannot take that.")
+    global inventory
+    if is_item_in_list(item_id, current_room["items"]):
+        remove_item_from_current_room(item_id, current_room["items"])
+        return inventory
+    else:
+        print("You cannot take that.")
 
 
 def drop_an_item(item_id):
-	"""This function allows the user to drop an item in their inventory
-	in the room that they're currently in."""
-	for item in inventory:
-		if item_id != item['id'] and current_room['items']:
-			pass
-		elif item_id == item['id']:
-			inventory.remove(item)
-			current_room['items'].append(item)
-			print("You dropped " + item['name'] + ".")
-		else:
-			print("You cannot drop that.")
+	"""
+	This function allows the user to take an item from their inventory.
+	It will again run two seperate functions like the take_an_item function but instead an item
+	is moved from the inventory and into the players current room.
+	Else You cannot drop that is displayed.
+	"""
+    if is_item_in_inventory(item_id, inventory):
+        remove_item_from_inventory(item_id, inventory)
+        return inventory
+    else:
+        print("You cannot drop that.")
 
 
 def use_item(item_id):
@@ -357,8 +464,17 @@ pass
 
 
 def execute_command(command):
-	"""This function takes a command and, depending on the type of 
-	action, executes either execute_GO, TAKE, DROP, USE."""
+	"""
+	This function takes a command that has been returned as a list of words after being normalised
+	and, depending on the type of action, executes either execute_GO, TAKE, DROP, USE.
+	For example:
+
+	'go south' would take the user to the room that is south of their current room.
+
+	'take biscuit' would take the item from the room and move it into the players inventory.
+
+	'drop broom' would take the item from the players inventory and add it to the current room. 
+	"""
 	if 0 == len(command):
 		return
 
@@ -390,9 +506,15 @@ def execute_command(command):
 		print("This is an invalid command.")
 
 def take_damage_player(player, damage):
-	"""This function deals damage to the player and updates their player stats.
+	"""
+	This function deals damage to the player and updates their player stats.
 	If the player's health goes below 0 they will die and a message will appear
-	indicating this."""
+	indicating this.
+	For example:
+	player["health"] = 0 then, You have died will be displayed.
+	However if,
+	player["health"] = 10 then, You have taken <damage> damage.
+	"""
 	player["health"] = player["health"] - damage
 	if player["health"] < 0:
 		player["health"] = 0
@@ -403,8 +525,10 @@ def take_damage_player(player, damage):
 
 
 def damage_dealt_by_npc(room):
-	"""This function takes the npc's name and see how much damage they deal.
-	The value returned is the amount of damage that has been dealt."""
+	"""
+	This function takes the npc's name and see how much damage they deal.
+	The value returned is the amount of damage that has been dealt.
+	"""
 	
 	npc_damage = ''
 	for y in room:
@@ -416,10 +540,11 @@ def damage_dealt_by_npc(room):
 				return npc_damage
 
 def npc_name(room):
-	"""This function returns the npc dictionary for an npc in a particular room.
+	"""
+	This function returns the npc dictionary for an npc in a particular room.
 	This looks in rooms, looks at npc's in room, looks at npcs attributes and 
-	returns their name."""
-
+	returns their name.
+	"""
 	
 	npc_name= ''
 	npc_list= ''
@@ -430,7 +555,9 @@ def npc_name(room):
 			return npc_name
 	
 def item_damage(item_id):
-	"""This function returns the damage value for an item."""
+	"""
+	This function returns the damage value for an item.
+	"""
 	for x in items:
 		if item_id == items['id']:
 			print(x)
@@ -438,19 +565,22 @@ def item_damage(item_id):
 
 def intoxication(text):
 	"""
-	This function causes each character to have a 1/5 chance of being replaced by a random letter from the string of letters
-
-
-	INSERT DOCTEST HERE
+	This function causes each character to have a 1/5 chance of being replaced by a random letter from the string of letters.
+	The string includes the description of the current room.
 	"""
+
 	string_letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	text = "".join(i if random.randint(0,4) else random.choice(string_letters) for i in text)	
 
 	return text
 
 def menu(exits, room_items, inventory_items):
-	"""This function calls the function """
-
+	"""
+	This function prints a menu of actions using the print_menu_of_items_and_exits function using
+	a dictionary of valid exits in the room and a list of items that can be found there or are carried
+	by the player. It will allow the player to enter a command and then normalises what they type, before 
+	returning it. 
+	"""
 	#display menu
 	print_menu_of_items_and_exits(exits, room_items, inventory_items)
 
@@ -464,12 +594,21 @@ def menu(exits, room_items, inventory_items):
 
 
 def move_to_another_room(exits, direction):
-	"""This function returns the room into which the player 
-	will move to."""
+	"""
+	This function returns the room into which the player 
+	will move to if the input contains a valid exit.
+	e.g.
+	>>> move_to_another_room(rooms["Lobby"]["exits"], "north") == rooms["Kitchen"]
+	True
+	>>> move_to_another_room(rooms["Bar"]["exits"], "north") == rooms["Lobby"]
+	False
+	"""
 	return room[exits[direction]]
 
 def stats(player, input_name):
-	"""This function displays the players stats."""
+	"""
+	This function displays the players stats for the user.
+	"""
 	print(" " + "_" * 25)
 	for x in player_stats:
 		if x == 'Name':
@@ -479,6 +618,8 @@ def stats(player, input_name):
 	print("|" + "_" * 25 + "|")
 	print(input("Press the enter key to continue."))
 
+
+#this function will run the entire game and will call the foundation functions. These in turn will call any other functions that they need.
 
 def main():
 	prompt_user = input("Please enter your name: ")
