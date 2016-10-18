9964644ba37e81a89dd9134c48f4429f22381499
Blobbypengu <ashleyt044@gmail.com>
2016-10-18 12:58:28 +0100

    removed starting pistol as having 2 in the game breaks the drop function

--- user.py
+++ user.py
@@ -2,7 +2,7 @@ from items import *
 from game_map import room
 
 #contains all the items that the user is carrying at any one time
-inventory = [item_pistol]
+inventory = []
 
 #contains the status of the player, their health etc.
 player = {"Name": "Change to input", "Health": 100, "Alive": True, "Intoxicated": False}
